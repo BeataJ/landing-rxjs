8d3c9e61d6057f4428ee74fe966d85a557c6f368
beataj <beata@emc2wave.com>
2021-05-18 15:27:20 -0700

    add getForecast() getCurrentLocation()

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'environment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,29 @@ import { Injectable } from '@angular/core';
 export class ForecastService {
 
   constructor() { }
+
+  getForecast() {
+    return this.getCurrentLocation()
+      .pipe(
+        map(coords => {
+          return new HttpParams()
+            .set('lat', String(coords.latitude))
+            .set('lon', String(coords.longitude))
+            .set('units', 'metric')
+            .set('appid', environment.apiKey)
+        })
+      )
+  }
+
+  getCurrentLocation() {
+    return new Observable<GeolocationCoordinates>((observer) => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete()
+        },
+        (err) => observer.error(err)
+      )
+    });
+  }
 }
