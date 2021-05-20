f930e88e08c9d510ab6f2e51a55f21d26c8631ef
beataj <beata@emc2wave.com>
2021-05-19 17:42:05 -0700

    add filter operator

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { map, switchMap, pluck, mergeMap } from 'rxjs/operators';
+import { map, switchMap, pluck, mergeMap, filter } from 'rxjs/operators';
 import { environment } from 'environment';
 
 
@@ -34,7 +34,8 @@ export class ForecastService {
         }),
         switchMap(params => this.http.get<OpenWeatherResponse>(this.url, { params })),
         pluck('list'),
-        mergeMap(value => of(...value))
+        mergeMap(value => of(...value)),
+        filter((value, index) => index % 8 === 0)
       )
   }
 
