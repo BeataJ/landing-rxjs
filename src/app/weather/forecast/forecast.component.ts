0c605ab611d712c4dbd2fd25a46c089ca1950872
beataj <beata@emc2wave.com>
2021-05-22 17:06:37 -0700

    add forecast$

--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ForecastService } from '../forecast.service'
 
 @Component({
@@ -7,13 +8,11 @@ import { ForecastService } from '../forecast.service'
   styleUrls: ['./forecast.component.css']
 })
 export class ForecastComponent implements OnInit {
-  forecastData = [];
+  forecast$: Observable;
 
   constructor(forcastServise: ForecastService) {
     forcastServise.getForecast()
-      .subscribe((forecastData) => {
-        this.forecastData = forecastData
-      })
+      
   }
 
   ngOnInit(): void {
