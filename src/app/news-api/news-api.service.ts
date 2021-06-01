6649551d657c7f4c1c14baa6660097aaa5cbfc2a
beataj <beata@emc2wave.com>
2021-05-31 18:17:02 -0700

    import Subject, tap, map, switchMap

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'environment'
+import { environment } from 'environment';
+import { Subject } from 'rxjs';
+import { tap, map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
