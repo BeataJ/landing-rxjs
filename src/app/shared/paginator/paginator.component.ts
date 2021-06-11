aef7c0a465d537d1de4dbd09201c128b790a8454
beataj <beata@emc2wave.com>
2021-06-10 17:21:58 -0700

    add @Input() numberOfPages: number;

--- src/app/shared/paginator/paginator.component.ts
+++ src/app/shared/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginatorComponent implements OnInit {
   // TODO: make sure we receive this value from the parent component
-  numberOfPages = 5;
-  currentPage = 1;
-
+  @Input() numberOfPages: number;
   pageOptions: number[];
+
+  currentPage = 1;
 
   constructor() {
     this.pageOptions = [
